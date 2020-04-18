fb53aba539b8dc596b058683012411343cb4ec29
Aaron <yanshaobo0230@163.com>
2020-04-18 10:34:34 +0800

    0 - 1

---
+++ YSSliderDemo/YSSliderDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  YSSliderDemo
+//
+//  Created by Aaron on 2020/4/18.
+//  Copyright © 2020 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ YSSliderDemo/YSSliderDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YSSliderDemo
+//
+//  Created by Aaron on 2020/4/18.
+//  Copyright © 2020 Aaron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
